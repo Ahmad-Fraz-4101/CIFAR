46696d9c11ed1a6a23bc9b003f2ef7dc04777061
Ahmad Fraz <l226735@lhr.nu.edu.pk.com>
2025-05-13 17:10:10 +0500

    coit

--- main.py
+++ main.py
@@ -41,10 +41,18 @@ app = FastAPI(title="CIFAR-10 Classifier API",
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://cifar-production.up.railway.app",
+        "http://localhost:8000",
+        "http://localhost:3000",
+        "http://127.0.0.1:8000",
+        "http://127.0.0.1:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=3600
 )
 
 # Define model path
